f2458ea8f5ea70269e11c02c099053876cc52b43
rrt <rrt_gogs@liveintellect.org>
2019-01-01 19:34:36 -0700

    Changed suitecrm_version to a variable in the script.

--- container_setup_suitecrm/suitecrm_install.sh
+++ container_setup_suitecrm/suitecrm_install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/qemu-arm-static /bin/sh
 
-wget --no-verbose https://suitecrm.com/files/160/SuiteCRM-7.10.11/335/SuiteCRM-7.10.11.zip
-unzip -q -d /opt SuiteCRM-7.10.11.zip
-ln -s /opt/SuiteCRM-7.10.11 /opt/suitecrm
-chown -R www-data:www-data /opt/SuiteCRM-7.10.11
+SUITECRM_VERSION=7.10.11
 
-# Several file permissions are not quite right after unzipping, so those need to be
-# fixed.
-chmod -R ugo+rw /opt/SuiteCRM-7.10.11/modules
-chmod -R ugo+rw /opt/SuiteCRM-7.10.11/upload
-chmod -R ugo+rw /opt/SuiteCRM-7.10.11/custom
-chmod -R ugo+rw /opt/SuiteCRM-7.10.11/cache
+# TODO: Find out if there is a way to not hard-code the subdirectories (e.g. 160 and 335 in this one)
+wget --no-verbose https://suitecrm.com/files/160/SuiteCRM-${SUITECRM_VERSION}/335/SuiteCRM-${SUITECRM_VERSION}.zip
+unzip -q -d /opt SuiteCRM-${SUITECRM_VERSION}.zip
+ln -s /opt/SuiteCRM-${SUITECRM_VERSION} /opt/suitecrm
+chown -R www-data:www-data /opt/SuiteCRM-${SUITECRM_VERSION}
+
+# Several file permissions are not quite right after unzipping,
+# so those need to be fixed.
+# TODO: Review/re-test this.  It might have been a file owner issue.
+chmod -R ugo+rw /opt/SuiteCRM-${SUITECRM_VERSION}/modules
+chmod -R ugo+rw /opt/SuiteCRM-${SUITECRM_VERSION}/upload
+chmod -R ugo+rw /opt/SuiteCRM-${SUITECRM_VERSION}/custom
+chmod -R ugo+rw /opt/SuiteCRM-${SUITECRM_VERSION}/cache
 
 # Also, presumably, we're done with the zip file now, so drop it to shrink the container
 # image a little.
-rm SuiteCRM-7.10.11.zip
+rm SuiteCRM-${SUITECRM_VERSION}.zip
