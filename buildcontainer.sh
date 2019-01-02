f26453e9c0186524ab7a955fe87a51fe7c9976eb
rrt <rrt_gogs@liveintellect.org>
2019-01-01 19:47:18 -0700

    Adding buildcontainer.sh

---
+++ buildcontainer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# build it without the version tag to replace the magic "latest" tag
+docker build -t rthomas67/rpi-suitecrm
+
+# tag it with the version number from the VERSION file
+version=`cat VERSION`
+echo "Tagging with version ${version}"
+docker tag -t rthomas67/rpi-suitecrm:${version} .
+
+echo "Ready for docker login, docker push"
