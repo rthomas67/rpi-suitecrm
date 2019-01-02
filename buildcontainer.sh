e3728c365504b7c33818dd5a0be3e3edd1a83aec
rrt <rrt_gogs@liveintellect.org>
2019-01-01 20:45:09 -0700

    corrected docker tag command

--- buildcontainer.sh
+++ buildcontainer.sh
@@ -6,6 +6,6 @@ docker build -t rthomas67/rpi-suitecrm .
 # tag it with the version number from the VERSION file
 version=`cat VERSION`
 echo "Tagging with version ${version}"
-docker tag -t rthomas67/rpi-suitecrm:${version}
+docker tag rthomas67/rpi-suitecrm rthomas67/rpi-suitecrm:${version}
 
 echo "Ready for docker login, docker push"
